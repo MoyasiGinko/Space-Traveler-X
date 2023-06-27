105b73497ec2977bbda2e26cf63a47e8ef961541
ahmed025566 <ahmedsalah025566@gmail.com>
2023-06-27 18:11:00 +0300

    Add browser router and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route, NavLink} from 'react-router'
+import Rockets from './components/rockets'
+import Missions from './components/missions'
+
 function App() {
 
   return (
-    <>
+  <>
+  
+  <header>
+    <nav className= "navBar">
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="/Missions">Missions</NavLink>
+    </nav>
+  </header>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={Rockets} />
+      <Route path='/Missions' element={Missions} />
+    </Routes>
+    </BrowserRouter> 
     </>
+    
   )
 }
 
