1d15ea297343a1abeda2dc4d1372646e49897fea
ahmed025566 <ahmedsalah025566@gmail.com>
2023-06-27 19:17:28 +0300

    Add logo and complete the header with it's styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { BrowserRouter,Routes, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter,Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
 import Rockets from './components/rockets'
 import Missions from './components/missions'
 import MyProfile from './components/myProfile'
+
 
 function App() {
 
   return (
   <BrowserRouter>
   
-  <header>
-    <nav className= "navBar">
-      <NavLink to="/">Rockets</NavLink>
-      <NavLink to="/Missions">Missions</NavLink>
-      <NavLink to="/MyProfile">My MyProfile</NavLink>
-    </nav>
-  </header>
-    
+  <NavBar />
+    <div className="devider"></div>
     <Routes>
       <Route path='/' element={<Rockets />} />
       <Route path='/Missions' element={<Missions />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+import planet from '../assets/planet.png'
+const NavBar = () => {
+  return (
+    <header className="header">
+        <div className="logoDiv">
+        <img src={planet} alt="logo" className="logo" />
+
+        <h1>Space Traveler&apos;s Hub</h1>
+        </div>
+    
+    <nav className= "navBar">
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="/Missions">Missions</NavLink>
+      <div className="line"></div>
+      <NavLink to="/MyProfile">MyProfile</NavLink>
+    </nav>
+  </header>
+  )
+}
+
+export default NavBar
