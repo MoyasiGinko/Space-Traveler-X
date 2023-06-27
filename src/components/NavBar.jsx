1b469ed514cb8295fc346840a49a870639a675b0
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-28 03:30:31 +0600

    Add activeClassName

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,21 +3,29 @@ import planet from '../assets/planet.png'
 const NavBar = () => {
   return (
     <header className="header">
-        <div className="logoDiv">
+      <div className="logoDiv">
         <img src={planet} alt="logo" className="logo" />
 
         <h1>Space Traveler&apos;s Hub</h1>
-        </div>
-    
-    <nav className= "navBar">
-      <NavLink to="/">Rockets</NavLink>
-      <NavLink to="/Missions">Missions</NavLink>
-      <NavLink to="/Dragons">Dragons</NavLink>
-      <div className="line"></div>
-      <NavLink to="/MyProfile">MyProfile</NavLink>
-    </nav>
-  </header>
-  )
+      </div>
+
+      <nav className="navBar">
+        <NavLink to="/" activeClassName="active">
+          Rockets
+        </NavLink>
+        <NavLink to="/Missions" activeClassName="active">
+          Missions
+        </NavLink>
+        <NavLink to="/Dragons" activeClassName="active">
+          Dragons
+        </NavLink>
+        <div className="line"></div>
+        <NavLink to="/MyProfile" activeClassName="active">
+          MyProfile
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
 
 export default NavBar
