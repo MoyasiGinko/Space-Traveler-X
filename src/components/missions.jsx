150620574ec5bb59e15924dd380d333528879eb9
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-28 23:32:06 +0600

    Fix linters

--- src/components/missions.jsx
+++ src/components/missions.jsx
@@ -1,12 +1,12 @@
-import { useSelector} from 'react-redux'
+import { useSelector } from 'react-redux';
 
 const Missions = () => {
-    const { test } = useSelector((state) => state.missions)
+  const { test } = useSelector((state) => state.missions);
   return (
     <div>
       <h1>{test}</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Missions;
