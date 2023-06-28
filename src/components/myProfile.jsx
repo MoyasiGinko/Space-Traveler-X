5ae2d9e0b3e3bd75dd116c7c3f40880a471323f4
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-29 00:50:53 +0600

    Separate the file and create child components

--- src/components/myProfile.jsx
+++ src/components/myProfile.jsx
@@ -1,54 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchDragons, cancelReservation } from '../redux/features/Dragons/dragonsSlice';
+import MyDragons from './myDragons';
 
 const MyProfile = () => {
-  const dispatch = useDispatch();
-  const { dragons, status } = useSelector((state) => state.dragons);
-
-  useEffect(() => {
-    if (status === 'idle') {
-      dispatch(fetchDragons());
-    }
-  }, [status, dispatch]);
-
-  const handleCancelReservation = (dragonId) => {
-    localStorage.removeItem(`reserved_${dragonId}`);
-    dispatch(cancelReservation(dragonId));
-  };
-
-  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
-
   return (
     <div>
       <h1>My Profile</h1>
-      {status === 'loading' ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {reservedDragons.length > 0 ? (
-            <ul>
-              {reservedDragons.map((dragon) => (
-                <li key={dragon.id}>
-                  <h4>{dragon.name}</h4>
-                  <p>{dragon.type}</p>
-                  <p>ID: {dragon.id}</p>
-                  <img
-                    className="dragon_img"
-                    src={dragon.flickr_images[1]}
-                    alt={dragon.name}
-                  />
-                  <button onClick={() => handleCancelReservation(dragon.id)}>
-                    Cancel Reservation
-                  </button>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No dragons reserved.</p>
-          )}
-        </>
-      )}
+      <MyDragons />
     </div>
   );
 };
