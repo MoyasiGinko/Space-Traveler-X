3746f19fdbb6f6ab0d8b41a550b66841a3a41f73
wwwdrt <juandapps@hotmail.com>
2023-06-28 10:01:48 -0500

    Update dispatch fetchRockets

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -1,10 +1,18 @@
-import { useSelector} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react';
+import { fetchRockets } from '../redux/features/Rockets/rocketSlice';
 
 const Rockets = () => {
   const { test } = useSelector((state) => state.rockets)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
     <div>
-    <h1>{test}</h1>
+      <h1>{test}</h1>
     </div>
   )
 }
