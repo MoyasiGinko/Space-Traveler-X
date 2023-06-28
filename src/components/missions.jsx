0a28532f4a45f0779e68d4a3d06f8bd351eb676b
wwwdrt <juandapps@hotmail.com>
2023-06-28 11:48:47 -0500

    Add mission joining action

--- src/components/missions.jsx
+++ src/components/missions.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchMissions } from '../redux/features/Missions/missionsSlice';
+import { fetchMissions, joinMission } from '../redux/features/Missions/missionsSlice';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions.missions);
@@ -10,12 +10,20 @@ const Missions = () => {
     dispatch(fetchMissions());
   }, [dispatch]);
 
+  const onJoinMissionClick = (id) => {
+    dispatch(joinMission(id));
+    const updatedMission = missions.find((mission) => mission.mission_id === id);
+    console.log(updatedMission);
+  }
+
+  console.log("Missions state: ", missions);
   return (
     <div>
-      {missions.map((mission) => (
+      {missions && missions.map((mission) => (
         <div key={mission.mission_id}>
           <h2>{mission.mission_name}</h2>
           <p>{mission.description}</p>
+          <button onClick={() => onJoinMissionClick(mission.mission_id)}>Join Mission</button>
         </div>
       ))}
     </div>
