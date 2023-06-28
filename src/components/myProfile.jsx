a2fbaa6406fdaee60001c4380bd431e1a4480734
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-29 00:45:51 +0600

    Fix the issue not-displaying after reload

--- src/components/myProfile.jsx
+++ src/components/myProfile.jsx
@@ -1,40 +1,53 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchDragons, cancelReservation } from '../redux/features/Dragons/dragonsSlice';
 
 const MyProfile = () => {
-  const { dragons } = useSelector((state) => state.dragons);
+  const dispatch = useDispatch();
+  const { dragons, status } = useSelector((state) => state.dragons);
 
-  const reservedDragons = dragons.filter((dragon) => {
-    const reserved = localStorage.getItem(`reserved_${dragon.id}`);
-    return reserved === 'true';
-  });
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchDragons());
+    }
+  }, [status, dispatch]);
 
   const handleCancelReservation = (dragonId) => {
     localStorage.removeItem(`reserved_${dragonId}`);
+    dispatch(cancelReservation(dragonId));
   };
+
+  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
 
   return (
     <div>
       <h1>My Profile</h1>
-      {reservedDragons.length > 0 ? (
-        <ul>
-          {reservedDragons.map((dragon) => (
-            <li key={dragon.id}>
-              <h4>{dragon.name}</h4>
-              <p>{dragon.type}</p>
-              <p>ID: {dragon.id}</p>
-              <img
-                className="dragon_img"
-                src={dragon.flickr_images[1]}
-                alt={dragon.name}
-              />
-              <button onClick={() => handleCancelReservation(dragon.id)}>
-                Cancel Reservation
-              </button>
-            </li>
-          ))}
-        </ul>
+      {status === 'loading' ? (
+        <p>Loading...</p>
       ) : (
-        <p>No dragons reserved.</p>
+        <>
+          {reservedDragons.length > 0 ? (
+            <ul>
+              {reservedDragons.map((dragon) => (
+                <li key={dragon.id}>
+                  <h4>{dragon.name}</h4>
+                  <p>{dragon.type}</p>
+                  <p>ID: {dragon.id}</p>
+                  <img
+                    className="dragon_img"
+                    src={dragon.flickr_images[1]}
+                    alt={dragon.name}
+                  />
+                  <button onClick={() => handleCancelReservation(dragon.id)}>
+                    Cancel Reservation
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No dragons reserved.</p>
+          )}
+        </>
       )}
     </div>
   );
