729508b21cd8ea19c1c08ece41f0c01d11b494bd
wwwdrt <juandapps@hotmail.com>
2023-06-28 09:53:59 -0500

    Set up missions slice

--- src/redux/features/Missions/missionsSlice.js
+++ src/redux/features/Missions/missionsSlice.js
@@ -1,15 +1,37 @@
-import { createSlice} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchMissions = createAsyncThunk(
+  'rockets/fetchMissions', async () => {
+    const response = await fetch('https://api.spacexdata.com/v3/missions');
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-    test: 'Missions'
+  rockets: [],
+  status: 'idle',
+  error: null,
 };
 
-const missionsSlice = createSlice({
-    name: 'missions',
-    initialState,
-    reducers: {
-
-    }
+const missionSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.rockets = action.payload;
+      })
+      .addCase(fetchMissions.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  }
 });
 
-export default missionsSlice.reducer;
+export default missionSlice.reducer;
