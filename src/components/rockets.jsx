9b20952c023e5908b0250d63495043966ec8cb20
wwwdrt <juandapps@hotmail.com>
2023-06-28 10:32:25 -0500

    Add display rockets

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { fetchRockets } from '../redux/features/Rockets/rocketSlice';
 
 const Rockets = () => {
-  const { test } = useSelector((state) => state.rockets)
+  const rockets = useSelector((state) => state.rockets.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -12,7 +12,13 @@ const Rockets = () => {
 
   return (
     <div>
-      <h1>{test}</h1>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <h2>{rocket.rocket_name}</h2>
+          <p>{rocket.description}</p>
+          <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+        </div>
+      ))}
     </div>
   )
 }
