a5a967b7461bcd04a5ace66924c1fae3bc082a9e
wwwdrt <juandapps@hotmail.com>
2023-06-29 10:08:07 -0500

    Track missions and reserverd rockets

--- src/components/myProfile.jsx
+++ src/components/myProfile.jsx
@@ -1,10 +1,38 @@
-import MyDragons from './child/myDragons';
+import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
+  const missions = useSelector((state) => state.missions.missions);
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
   return (
-    <div>
-      <h1>My Profile</h1>
-      <MyDragons />
+    <div id='myprofile'>
+      <div id='myprofile-missions'>
+        <h2>My Missions</h2>
+        <table>
+          <tbody>
+            {joinedMissions.map(mission => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div id='myprofile-rockets'>
+        <h2>My Rockets</h2>
+        <table>
+          <tbody>
+            {reservedRockets.map(rocket => (
+              <tr key={rocket.id}>
+                <td>{rocket.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
