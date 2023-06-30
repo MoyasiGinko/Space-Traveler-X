b76932e53e0bf50671e8656547f1ecd6ab202bf6
wwwdrt <juandapps@hotmail.com>
2023-06-30 17:10:50 -0500

    Update missions component

--- src/components/missions.jsx
+++ src/components/missions.jsx
@@ -10,33 +10,64 @@ const Missions = () => {
     dispatch(fetchMissions());
   }, [dispatch]);
 
-  
-
   return (
     <div id='missions'>
-      {missions && missions.map((mission) => (
-        <div key={mission.mission_id} id='card'>
-          <h2>{mission.mission_name}</h2>
-          {mission.reserved ? (
-            <span className='active-member'>ACTIVE MEMBER</span>
-          ) : (
-            <span className='not-a-member'>NOT A MEMBER</span>
-          )}
-          <p>{mission.description}</p>
-          <button
-            onClick={() => {
-              if (mission.reserved) {
-                dispatch(leaveMission(mission.mission_id));
-              } else {
-                dispatch(joinMission(mission.mission_id));
-              }
-            }}
-            className='mission-btn'
-          >
-            {mission.reserved ? "Leave Mission" : "Join Mission"}
-          </button>
-        </div>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions && missions.map((mission) => (
+            <tr key={mission.mission_id} id='card'>
+              <td id='mission-name'>{mission.mission_name}</td>
+              <td id='mission-description'>{mission.description}</td>
+              <td id='mission-status'>
+                {mission.reserved ? (
+                  <span className='active-member'>ACTIVE MEMBER</span>
+                ) : (
+                  <span className='not-a-member'>NOT A MEMBER</span>
+                )}
+              </td>
+              <td id='mission-btn-col'>
+                {mission.reserved ? (
+                  <button
+                    onClick={() => {
+                      if (mission.reserved) {
+                        dispatch(leaveMission(mission.mission_id));
+                      } else {
+                        dispatch(joinMission(mission.mission_id));
+                      }
+                    }}
+                    className='join-mission-btn'
+                  >
+                    Leave Mission
+                  </button>
+                )
+                  : (
+                    <button
+                      onClick={() => {
+                        if (mission.reserved) {
+                          dispatch(leaveMission(mission.mission_id));
+                        } else {
+                          dispatch(joinMission(mission.mission_id));
+                        }
+                      }}
+                      className='leave-mission-btn'
+                    >
+                      Join Mission
+                    </button>
+                  )
+                }
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
